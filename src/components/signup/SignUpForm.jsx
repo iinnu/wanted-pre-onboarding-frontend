6906a7dc7cceb80601754ab362051fa0991e3894
Inhu Jo <inhudev@gmail.com>
2023-06-11 02:37:39 +0900

    feat: 회원가입 폼 구현
    - 회원가입 폼 추가
    - 회원가입 api 요청 추가

---
+++ src/components/signup/SignUpForm.jsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useField from 'hook/useField';
+import apiInstance from 'api';
+
+const emailRegex = /.@./;
+const passwordRegex = /.{8,}/;
+
+const SignUpForm = () => {
+  const [email, handleEmailChange] = useField('');
+  const [password, handlePasswordChange] = useField('');
+  const [disabled, setDisabled] = useState(true);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const isSignUp = await apiInstance.post('/auth/signup', {
+        email,
+        password,
+      });
+
+      if (isSignUp.status === 201) {
+        alert('회원가입이 완료되었습니다!');
+        navigate('/signin');
+      }
+    } catch {
+      alert('이미 존재하는 이메일이거나 형식이 맞지 않습니다.');
+    }
+  };
+
+  // 유효성 검사
+  useEffect(() => {
+    const isEmailOk = emailRegex.test(email);
+    const isPasswordOk = passwordRegex.test(password);
+    if (isEmailOk && isPasswordOk) {
+      setDisabled(false);
+    }
+  }, [email, password]);
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input
+          data-testid='email-input'
+          type='email'
+          value={email}
+          onChange={handleEmailChange}
+        />
+        <input
+          data-testid='password-input'
+          type='password'
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button data-testid='signup-button' type='submit' disabled={disabled}>
+          회원가입
+        </button>
+      </form>
+      <a href='/signin'>로그인하기</a>
+    </>
+  );
+};
+
+export default SignUpForm;
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,5 +1,12 @@
+import SignUpForm from 'components/signup/SignUpForm';
+
 const SignUpPage = () => {
-  return <div>signup page</div>;
+  return (
+    <>
+      <h1>회원가입</h1>
+      <SignUpForm />
+    </>
+  );
 };
 
 export default SignUpPage;
