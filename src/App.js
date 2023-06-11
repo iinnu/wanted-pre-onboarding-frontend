d98039680c75fad968b0417a4952d14876f9bf40
Inhu Jo <inhudev@gmail.com>
2023-06-11 20:57:08 +0900

    feat: github pages 관련 설정 삭제

--- src/App.js
+++ src/App.js
@@ -9,27 +9,24 @@ import TodoPage from 'pages/TodoPage';
 import PublicAuth from 'auth/PublicAuth';
 import PrivateAuth from 'auth/PrivateAuth';
 
-const router = createBrowserRouter(
-  [
-    {
-      path: '/',
-      element: <Navigate replace to='/signin' />,
-    },
-    {
-      path: '/signin',
-      element: <PublicAuth Component={SignInPage} redirectURL='/todo' />,
-    },
-    {
-      path: '/signup',
-      element: <PublicAuth Component={SignUpPage} redirectURL='/todo' />,
-    },
-    {
-      path: '/todo',
-      element: <PrivateAuth Component={TodoPage} redirectURL='/signin' />,
-    },
-  ],
-  { basename: process.env.PUBLIC_URL }
-);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate replace to='/signin' />,
+  },
+  {
+    path: '/signin',
+    element: <PublicAuth Component={SignInPage} redirectURL='/todo' />,
+  },
+  {
+    path: '/signup',
+    element: <PublicAuth Component={SignUpPage} redirectURL='/todo' />,
+  },
+  {
+    path: '/todo',
+    element: <PrivateAuth Component={TodoPage} redirectURL='/signin' />,
+  },
+]);
 
 const App = () => {
   return <RouterProvider router={router} />;
