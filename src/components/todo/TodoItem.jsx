86ee2ab3519fb914bcc0c6618e2b1774bb2e1fca
Inhu Jo <inhudev@gmail.com>
2023-06-11 18:18:09 +0900

    feat: 투두 수정 기능 구현

--- src/components/todo/TodoItem.jsx
+++ src/components/todo/TodoItem.jsx
@@ -1,6 +1,10 @@
+import { useState } from 'react';
 import apiInstance from 'api';
 
 const TodoItem = ({ todo, onTodoDelete, onTodoUpdate }) => {
+  const [isUpdate, setIsUpdate] = useState(false);
+  const [updatedTodo, setUpdatedTodo] = useState('');
+
   const handleTodoDelete = async () => {
     try {
       const todoDelete = await apiInstance.delete(`/todos/${todo.id}`);
@@ -13,16 +17,57 @@ const TodoItem = ({ todo, onTodoDelete, onTodoUpdate }) => {
     }
   };
 
+  const handleTodoUpdate = async () => {
+    try {
+      const todoUpdate = await apiInstance.put(`/todos/${todo.id}`, {
+        todo: updatedTodo,
+        isCompleted: todo.isCompleted,
+      });
+
+      if (todoUpdate.status === 200) {
+        onTodoUpdate(todoUpdate.data);
+        setIsUpdate(false);
+        setUpdatedTodo('');
+      }
+    } catch {
+      alert('투두 수정에 실패했습니다. 다시 시도해주세요.');
+    }
+  };
+
   return (
     <li>
       <label>
         <input type='checkbox' />
         <span>{todo.todo}</span>
       </label>
-      <button data-testid='modify-button'>수정</button>
-      <button data-testid='delete-button' onClick={handleTodoDelete}>
-        삭제
-      </button>
+      {!isUpdate && (
+        <>
+          <button data-testid='modify-button' onClick={() => setIsUpdate(true)}>
+            수정
+          </button>
+          <button data-testid='delete-button' onClick={handleTodoDelete}>
+            삭제
+          </button>
+        </>
+      )}
+      {isUpdate && (
+        <>
+          <input
+            data-testid='modify-input'
+            value={updatedTodo}
+            onChange={(e) => setUpdatedTodo(e.target.value)}
+          />
+          <button data-testid='submit-button' onClick={handleTodoUpdate}>
+            제출
+          </button>
+          <button
+            data-testid='cancel-button'
+            onClick={() => setIsUpdate(false)}
+          >
+            취소
+          </button>
+        </>
+      )}
     </li>
   );
 };
